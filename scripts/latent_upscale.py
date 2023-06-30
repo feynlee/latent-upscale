1728233d50cf97961d0a58344ec4b8630b4fef87
Ziyue Li <liziyue241@gmail.com>
2023-06-29 20:35:09 -0400

    get the init_latent

--- scripts/latent_upscale.py
+++ scripts/latent_upscale.py
@@ -1,11 +1,15 @@
+import cv2
 import modules.scripts as scripts
+import modules.shared as shared
 import gradio as gr
+import numpy as np
 import torch
 import os
+import hashlib
 
-from modules import images
-from modules.processing import process_images, Processed
-from modules.processing import Processed
+from PIL import Image, ImageOps
+from modules import images, masking
+from modules.processing import process_images, Processed, setup_color_correction, apply_color_correction
 from modules.shared import opts, cmd_opts, state
 
 
@@ -49,8 +53,112 @@ class Script(scripts.Script):
 # what is returned by the process_images method.
 
     def run(self, p, upscale_method):
+        # Do the same procedures as defined in StableDiffusionProcessingImg2Img's __init__
+        # to get the init_latent
+        image_mask = p.image_mask
 
+        if image_mask is not None:
+            image_mask = image_mask.convert('L')
+
+            if p.inpainting_mask_invert:
+                image_mask = ImageOps.invert(image_mask)
+
+            if p.mask_blur_x > 0:
+                np_mask = np.array(image_mask)
+                kernel_size = 2 * int(4 * p.mask_blur_x + 0.5) + 1
+                np_mask = cv2.GaussianBlur(np_mask, (kernel_size, 1), p.mask_blur_x)
+                image_mask = Image.fromarray(np_mask)
+
+            if p.mask_blur_y > 0:
+                np_mask = np.array(image_mask)
+                kernel_size = 2 * int(4 * p.mask_blur_y + 0.5) + 1
+                np_mask = cv2.GaussianBlur(np_mask, (1, kernel_size), p.mask_blur_y)
+                image_mask = Image.fromarray(np_mask)
+
+            if p.inpaint_full_res:
+                p.mask_for_overlay = image_mask
+                mask = image_mask.convert('L')
+                crop_region = masking.get_crop_region(np.array(mask), p.inpaint_full_res_padding)
+                crop_region = masking.expand_crop_region(crop_region, p.width, p.height, mask.width, mask.height)
+                x1, y1, x2, y2 = crop_region
+
+                mask = mask.crop(crop_region)
+                image_mask = images.resize_image(2, mask, p.width, p.height)
+                p.paste_to = (x1, y1, x2-x1, y2-y1)
+            else:
+                image_mask = images.resize_image(p.resize_mode, image_mask, p.width, p.height)
+                np_mask = np.array(image_mask)
+                np_mask = np.clip((np_mask.astype(np.float32)) * 2, 0, 255).astype(np.uint8)
+                p.mask_for_overlay = Image.fromarray(np_mask)
+
+            p.overlay_images = []
+
+        latent_mask = p.latent_mask if p.latent_mask is not None else image_mask
+
+        add_color_corrections = opts.img2img_color_correction and p.color_corrections is None
+        if add_color_corrections:
+            p.color_corrections = []
+        imgs = []
+        for img in p.init_images:
+
+            # Save init image
+            if opts.save_init_img:
+                p.init_img_hash = hashlib.md5(img.tobytes()).hexdigest()
+                images.save_image(img, path=opts.outdir_init_images, basename=None, forced_filename=p.init_img_hash, save_to_dirs=False)
+
+            image = images.flatten(img, opts.img2img_background_color)
+
+            if crop_region is None and p.resize_mode != 3:
+                image = images.resize_image(p.resize_mode, image, p.width, p.height)
+
+            if image_mask is not None:
+                image_masked = Image.new('RGBa', (image.width, image.height))
+                image_masked.paste(image.convert("RGBA").convert("RGBa"), mask=ImageOps.invert(p.mask_for_overlay.convert('L')))
+
+                p.overlay_images.append(image_masked.convert('RGBA'))
+
+            # crop_region is not None if we are doing inpaint full res
+            if crop_region is not None:
+                image = image.crop(crop_region)
+                image = images.resize_image(2, image, p.width, p.height)
+
+            if image_mask is not None:
+                if p.inpainting_fill != 1:
+                    image = masking.fill(image, latent_mask)
+
+            if add_color_corrections:
+                p.color_corrections.append(setup_color_correction(image))
+
+            image = np.array(image).astype(np.float32) / 255.0
+            image = np.moveaxis(image, 2, 0)
+
+            imgs.append(image)
+
+        if len(imgs) == 1:
+            batch_images = np.expand_dims(imgs[0], axis=0).repeat(p.batch_size, axis=0)
+            if p.overlay_images is not None:
+                p.overlay_images = p.overlay_images * p.batch_size
+
+            if p.color_corrections is not None and len(p.color_corrections) == 1:
+                p.color_corrections = p.color_corrections * p.batch_size
+
+        elif len(imgs) <= p.batch_size:
+            p.batch_size = len(imgs)
+            batch_images = np.array(imgs)
+        else:
+            raise RuntimeError(f"bad number of images passed: {len(imgs)}; expecting {p.batch_size} or less")
+
+        image = torch.from_numpy(batch_images)
+        image = 2. * image - 1.
+        image = image.to(shared.device)
+
+        self.init_latent = self.sd_model.get_first_stage_encoding(self.sd_model.encode_first_stage(image))
+
+
+        # Upscale the latent space to the desired resolution with options to choose the method
         p.init_latent = torch.nn.functional.interpolate(p.init_latent, size=(p.height // opt_f, p.width // opt_f), mode=upscale_method)
+
+        # Process the Images
         proc = process_images(p)
 
         return proc

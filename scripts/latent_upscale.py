30468de0a56281394105e5b9b8c1c748f52f908f
Ziyue Li <liziyue241@gmail.com>
2023-07-12 17:27:49 -0400

    user create_sampler

--- scripts/latent_upscale.py
+++ scripts/latent_upscale.py
@@ -14,6 +14,7 @@ from modules import images, masking, sd_samplers_common
 from modules.processing import setup_color_correction, create_random_tensors
 from modules.shared import opts, state
 from modules.sd_samplers import find_sampler_config
+from modules import sd_samplers
 from modules.sd_samplers_kdiffusion import CFGDenoiser, TorchHijack, sampler_extra_params, k_diffusion_scheduler
 
 
@@ -373,9 +374,9 @@ class Script(scripts.Script):
 
         # override the init method
         def init(all_prompts, all_seeds, all_subseeds, **kwargs):
-            # p.sampler = sd_samplers.create_sampler(p.sampler_name, p.sd_model)
-            p.sampler = KDiffusionSampler(samplers_k_diffusion_dict[p.sampler_name], p.sd_model)
-            p.sampler.config = find_sampler_config(p.sampler_name)
+            p.sampler = sd_samplers.create_sampler(p.sampler_name, p.sd_model)
+            # p.sampler = KDiffusionSampler(samplers_k_diffusion_dict[p.sampler_name], p.sd_model)
+            # p.sampler.config = find_sampler_config(p.sampler_name)
 
             crop_region = None
 
